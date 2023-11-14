13cc7b0cf09b5b58de9c8e1e1e45f5ed1b897dbe
Alexander Solovkin <solovkin1990@gmail.com>
2023-11-14 17:43:18 +0300

    tweak(code): Edited ProductionBuilding, ProductionTime to add sliders. tweak(scene): Added sliders to buttons to display coroutine's progress. tweak(visual): Made buttons inactive during coroutine.

--- Assets/_Source/Game/ProductionBuilding.cs
+++ Assets/_Source/Game/ProductionBuilding.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using _Source.Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Source.Game
 {
--- Assets/_Source/Game/ProductionTime.cs
+++ Assets/_Source/Game/ProductionTime.cs
@@ -1,22 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 namespace _Source.Game
 {
   public class ProductionTime : MonoBehaviour
   {
+    [SerializeField]
+    private Button _button;
+    
+    [SerializeField]
+    private Slider _slider;
+    
     public float CooldownTime { get; set; }
     public bool CanDoAction{ get; set; }
     public void StartTimer() => StartCoroutine(Cooldown());
     private void Start()
     {
+      _slider.enabled = false;
       CanDoAction = true;
       CooldownTime = 3;
     }
     private IEnumerator Cooldown()
     {
+      _button.enabled = false;
       CanDoAction = false;
-      yield return new WaitForSeconds(CooldownTime);
+      for (float invincibleTimeLeft = CooldownTime; invincibleTimeLeft > 0; invincibleTimeLeft -= Time.deltaTime)
+      {
+        yield return null;
+        _slider.value = (CooldownTime - invincibleTimeLeft)/CooldownTime;
+      }
+      _slider.value = 0;
       CanDoAction = true;
+      _button.enabled = true;
     }
   }
 }
